b383fd72c956f552cb2053155645c2ce4a22f97b
Jado <jade.bouiges@edu.itescia.fr>
2023-04-29 09:12:49 +0200

    feat: add pauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+
+    [SerializeField] GameObject _pauseMenuUI;
+    [SerializeField] GameObject _settingsWindow;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Paused()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+
+    public void SettingsWindow()
+    {
+        _settingsWindow.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("SceneMenu");
+    }
+}
