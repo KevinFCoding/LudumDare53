118d4cfafc2451e0e6bb4856ae7549d6a160d17c
Jado <jade.bouiges@edu.itescia.fr>
2023-04-29 22:22:11 +0200

    add defeat panel

--- Assets/EndingManager.cs
+++ Assets/EndingManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class EndingManager : MonoBehaviour
 {
+    [Header("Victory")]
 
     [SerializeField] GameObject _winParticules;
     [SerializeField] GameObject _victoryPanel;
     [SerializeField] GameObject _victoryImage;
+
+
+    [Header("Defeate")]
+
+    [SerializeField] GameObject _looseParticules;
+    [SerializeField] GameObject _loosePanel;
+    [SerializeField] GameObject _looseImage;
     void Start()
     {
-        Win();
+        print("Start");
+        //  Win();
+        Loose();
     }
 
     void Update()
@@ -18,14 +28,36 @@ public class EndingManager : MonoBehaviour
         
     }
 
+    public void Loose()
+    {
+
+        print("looseFunction");
+
+        _loosePanel.SetActive(true);
+        StartCoroutine(ActiveEndingLoosePanel());
+    }
+
     public void Win()
     {
         _victoryPanel.SetActive(true);
-        StartCoroutine(ActiveEndingPanel());
+        StartCoroutine(ActiveEndingWinPanel());
     }
    
 
-    IEnumerator ActiveEndingPanel()
+    IEnumerator ActiveEndingLoosePanel()
+    {
+        print("looseCorouutine");
+
+        yield return new WaitForSeconds(1.6f);
+        _looseImage.SetActive(true);
+        print("looseImage");
+        yield return new WaitForSeconds(0.5f);
+        _looseParticules.SetActive(true);
+
+        yield break;
+    }
+
+    IEnumerator ActiveEndingWinPanel()
     {
         yield return new WaitForSeconds(1.6f);
         _victoryImage.SetActive(true);
